3091c204e216d9cec949663442fcec539a6a2394
Bryan Cho <bryanc208@gmail.com>
2022-04-07 22:04:13 -0400

    simplify coding starter even more

--- src/components/pages/VotePage.tsx
+++ src/components/pages/VotePage.tsx
@@ -1,6 +1,3 @@
-import Body1 from "components/text/Body1";
-import Body1Bold from "components/text/Body1Bold";
-import Header1 from "components/text/Header1";
 import EXAMPLE_SUBMISSIONS, { Submission } from "constants/ExampleSubmissions";
 import styles from "css/pages/VotePage.module.css";
 
@@ -9,15 +6,24 @@ export default function VotePage(): JSX.Element {
     EXAMPLE_SUBMISSIONS[Math.floor(Math.random() * EXAMPLE_SUBMISSIONS.length)];
   console.log(randomExample);
 
+  // If desired, utility fonts have been pre-defined in FontClasses.css
   return (
     <div className={styles.container}>
       <div className={styles.example}>
-        <Header1>Welcome!</Header1>
-        <Body1 className={styles.inline}>
-          To get started, take a look at <Body1Bold>Routes.tsx.</Body1Bold>
-        </Body1>
-        <Body1>Check your console for the data of an example submission.</Body1>
-        <Body1Bold>Good luck!</Body1Bold>
+        <div className="header1Font">Welcome!</div>
+        <ul>
+          <li className="body1Font">
+            You&apos;re currently looking at <strong>VotePage.tsx</strong>
+          </li>
+          <li className="body1Font">
+            Local storage helpers have been defined for your convenience under{" "}
+            <strong>utils/local-storage</strong>
+          </li>
+          <li className="body1Font">
+            Check your JS console for the data of an example submission.
+          </li>
+        </ul>
+        <div className="body1Font">Good luck!</div>
       </div>
     </div>
   );
--- src/utils/joinClasses.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Joins the passed-in CSS classes with a space.
- */
-export default function joinClasses(
-  ...cssClasses: Array<string | null | undefined>
-): string {
-  return cssClasses.filter((className) => className != null).join(" ");
-}
