af0dbf43d6f26e840eff021cc5bd7417c69a09c1
Bryan Cho <bryanc208@gmail.com>
2022-08-16 15:24:45 -0400

    Simplify coding starter even more

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import "css/global/Global.css";
 
-// Fonts
-import "css/global/fonts/FontClasses.css";
-import "css/global/fonts/FontVariables.css";
+import { Route, Routes as RoutesImport, BrowserRouter } from "react-router-dom";
+import EXAMPLE_SUBMISSIONS, { Submission } from "ExampleSubmissions";
+import styles from "css/pages/VotePage.module.css";
 
-import Routes from "routes/Routes";
-import { BrowserRouter } from "react-router-dom";
+function VotePage(): JSX.Element {
+  const randomExample: Submission =
+    EXAMPLE_SUBMISSIONS[Math.floor(Math.random() * EXAMPLE_SUBMISSIONS.length)];
+  console.log(randomExample);
+
+  // If desired, utility fonts have been pre-defined in FontClasses.css
+  return (
+    <div className={styles.container}>
+      <div className={styles.example}>
+        <h1>Welcome!</h1>
+        <ul>
+          <li>
+            You&apos;re currently looking at <strong>VotePage.tsx</strong>
+          </li>
+          <li>
+            Local storage helpers have been defined for your convenience under{" "}
+            <strong>utils/local-storage</strong>
+          </li>
+          <li>Check your JS console for the data of an example submission.</li>
+        </ul>
+        <div>Good luck!</div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <RoutesImport>
+        <Route path="/" element={<VotePage />} />
+      </RoutesImport>
     </BrowserRouter>
   );
 }
